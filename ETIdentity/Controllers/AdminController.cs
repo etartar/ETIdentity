c4f968987b8aa62051aba7589d3164522a52934c
Emir TARTAR <emirtartar35@gmail.com>
2021-12-11 12:42:44 +0300

    AspNetUsers tablosundaki üye bilgilerinin gösterilmesi

---
+++ ETIdentity/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using ETIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ETIdentity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        
+        public IActionResult Index()
+        {
+            return View(_userManager.Users.ToList());
+        }
+    }
+}
