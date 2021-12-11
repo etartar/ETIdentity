e4423b695517cf66a08a5657c6142baad02a1328
Emir TARTAR <emirtartar35@gmail.com>
2021-12-11 20:46:17 +0300

    Custom şifre doğrulama mekanizması

---
+++ ETIdentity/CustomValidations/CustomPasswordValidator.cs
@@ -0,0 +1,51 @@
+﻿using ETIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ETIdentity.CustomValidations
+{
+    public class CustomPasswordValidator : IPasswordValidator<AppUser>
+    {
+        public async Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user, string password)
+        {
+            List<IdentityError> errors = new List<IdentityError>();
+
+            if (password.ToLower().Contains(user.UserName.ToLower()))
+            {
+                errors.Add(new IdentityError()
+                {
+                    Code = "PasswordContainsUserName",
+                    Description = "Şifre alanı kullanıcı adı içeremez."
+                });
+            }
+
+            if (password.ToLower().Contains("1234"))
+            {
+                errors.Add(new IdentityError()
+                {
+                    Code = "PasswordContains1234",
+                    Description = "Şifre alanı ardışık sayı içeremez."
+                });
+            }
+
+            if (password.ToLower().Contains(user.Email.ToLower()))
+            {
+                errors.Add(new IdentityError()
+                {
+                    Code = "PasswordContainsEmail",
+                    Description = "Şifre alanı email adres içeremez."
+                });
+            }
+
+            if (errors.Count == 0)
+            {
+                return await Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return await Task.FromResult(IdentityResult.Failed(errors.ToArray()));
+            }
+        }
+    }
+}
